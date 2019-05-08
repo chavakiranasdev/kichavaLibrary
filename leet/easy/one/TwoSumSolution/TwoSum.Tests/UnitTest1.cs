112761f843a175e24756b8a50a3e671e871b2057
Kiran Chava <chavakiran.as.dev@gmail.com>
2019-05-08 16:24:57 -0700

    Adding another solution and updating unit tests.
    This is final commit to this solution.

--- leet/easy/one/TwoSumSolution/TwoSum.Tests/UnitTest1.cs
+++ leet/easy/one/TwoSumSolution/TwoSum.Tests/UnitTest1.cs
@@ -9,16 +9,24 @@ namespace TwoSum.Tests
     public class UnitTest1
     {
         [TestMethod]
-        [DataRow(new int[] { 4, 5, 6, 7, 8 }, 12, new int[] { 0, 4 })]
-        [DataRow(new int[] { 4, 5, 6, 7, 8 }, 9, new int[] { 0, 1 })]
-        [DataRow(new int[] { 4, 5, 6, 7, 8 }, null, null)]
-        [DataRow(new int[] { }, 9, null)]
-        [DataRow(new int[] { 9 }, 9, new int[] { 0 })]
-        [DataRow(new int[] { Int32.MinValue, Int32.MaxValue, 1 }, Int32.MaxValue, null)]
-        [DataRow(null, null, null)]
-        public void TwoSum_Positive_ShouldWork(int[] nums, int target, int[] expected)
+        [DataRow(new int[] { 4, 5, 6, 1, 8 }, 12, new int[] { 0, 4 }, AlgorithmToUse.BruteForce)]
+        [DataRow(new int[] { 4, 5, 6, 1, 8 }, 9, new int[] { 0, 1 }, AlgorithmToUse.BruteForce)]
+        [DataRow(new int[] { 4, 5, 6, 1, 8 }, null, null, AlgorithmToUse.BruteForce)]
+        [DataRow(new int[] { }, 9, null, AlgorithmToUse.BruteForce)]
+        [DataRow(new int[] { 9 }, 9, new int[] {0}, AlgorithmToUse.BruteForce)]
+        [DataRow(new int[] {Int32.MinValue, Int32.MaxValue, 1}, Int32.MaxValue, null, AlgorithmToUse.BruteForce)]
+        [DataRow(null, null, null, AlgorithmToUse.BruteForce)]
+
+        [DataRow(new int[] { 4, 5, 6, 1, 8 }, 12, new int[] { 0, 4 }, AlgorithmToUse.HashOnePass)]
+        [DataRow(new int[] { 4, 5, 6, 1, 8 }, 9, new int[] { 0, 1 }, AlgorithmToUse.HashOnePass)]
+        [DataRow(new int[] { 4, 5, 6, 1, 8 }, null, null, AlgorithmToUse.HashOnePass)]
+        [DataRow(new int[] { }, 9, null, AlgorithmToUse.HashOnePass)]
+        [DataRow(new int[] { 9 }, 9, new int[] {0}, AlgorithmToUse.HashOnePass)]
+        [DataRow(new int[] {Int32.MinValue, Int32.MaxValue, 1}, Int32.MaxValue, null, AlgorithmToUse.HashOnePass)]
+        [DataRow(null, null, null, AlgorithmToUse.HashOnePass)]
+        public void TwoSum_Positive_ShouldWork(int[] nums, int target, int[] expected, AlgorithmToUse algorithmToUse)
         {
-            var actual = new Solution().TwoSum(nums, target);
+            var actual = new Solution().TwoSum(nums, target, algorithmToUse);
             Assert.AreEqual(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual), $"Unexpected output. Expected: {JsonConvert.SerializeObject(expected)} but actual is " +
                 $"{JsonConvert.SerializeObject(actual)} ");
         }
--- leet/easy/one/TwoSumSolution/TwoSum/Solution.cs
+++ leet/easy/one/TwoSumSolution/TwoSum/Solution.cs
@@ -1,23 +1,55 @@
 namespace KichavaLibrary.Leet.Easy.TwoSum
 {
     using System;
+    using System.Collections.Generic;
 
     public enum AlgorithmToUse
     {
         BruteForce,
+        HashOnePass,
     }
+
     public class Solution
     {
-        public int[] TwoSum(int[] nums, int target, AlgorithmToUse algorithmToUse = AlgorithmToUse.BruteForce)
+        public int[] TwoSum(int[] nums, int target, AlgorithmToUse algorithmToUse = AlgorithmToUse.HashOnePass)
         {
             switch (algorithmToUse)
             {
                 case AlgorithmToUse.BruteForce:
                     return BruteForce(nums, target);
+                case AlgorithmToUse.HashOnePass:
+                    return HashOnePass(nums, target);
                 default:
                     return BruteForce(nums, target);
             }
         }
+
+        private int[] HashOnePass(int[] nums, int target)
+        {
+            if (nums == null) return null;
+            int length = nums.Length;
+            if (length == 1)
+            {
+                if (target != nums[0])
+                {
+                    return null;
+                }
+                return new int[] { 0 };
+            }
+
+            var hash = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (hash.ContainsKey(diff))
+                {
+                    return new int[] { hash[diff], i };
+                }
+                hash[nums[i]] = i;
+            }
+            return null;
+        }
+
         private int[] BruteForce(int[] nums, int target)
         {
             if (nums == null) return null;
