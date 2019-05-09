15ae791e1caedd0a85a2743c08644595b8187de7
Kiran Chava <chavakiran.as.dev@gmail.com>
2019-05-09 07:36:57 -0700

    Remove not used code.

--- leet/moderate/Two/AddNumLinkedListSolution/AddNumLinkedList/Solution.cs
+++ leet/moderate/Two/AddNumLinkedListSolution/AddNumLinkedList/Solution.cs
@@ -19,9 +19,6 @@ namespace KichavaLibrary.Leet.Moderate.AddNumLinkedList
 
             ListNode l1Head = l1;
             ListNode l2head = l2;
-            int length1, length2;
-
-            GetLengths(l1, l2, out length1, out length2);
 
             ListNode sum = new ListNode(0);
             ListNode sumMover = sum;
@@ -63,24 +60,6 @@ namespace KichavaLibrary.Leet.Moderate.AddNumLinkedList
                 throw new Exception("Bug we have left overs in one of lists");
             }
             return sum.next;
-        }
-        private void GetLengths(ListNode l1, ListNode l2, out int length1, out int length2)
-        {
-            length1 = 0;
-            length2 = 0;
-            do
-            {
-                if (l1 != null)
-                {
-                    length1++;
-                    l1 = l1.next;
-                }
-                if (l2 != null)
-                {
-                    length2++;
-                    l2 = l2.next;
-                }
-            } while (l1 != null || l2 != null);
         }
     }
 }
