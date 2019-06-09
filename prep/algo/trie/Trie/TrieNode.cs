1bd22fd8b668fd5cb481e44c5c252f0b261f9984
Kiran Chava <chavakiran.as.dev@gmail.com>
2019-06-09 13:21:01 -0700

    First Version of Trie Insert, Delete, IsPresent, DFS

---
+++ prep/algo/trie/Trie/TrieNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Kichava.Library.Trie
+{
+    using System;
+    using System.Collections.Generic;
+    public class TrieNode<T>
+    {
+        public bool IsComplete { get; set; }
+
+        public Dictionary<char, TrieNode<T>> Children { get; set; }
+
+        public T CompletePathValue { get; set; }
+    }
+}
